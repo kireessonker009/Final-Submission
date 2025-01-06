e639708ffdb00fad7108a4334deaab3955794dcc
kireessonker009 <kireessonker00@gmail.com>
2025-01-06 15:35:14 +0530

    Create RegisterServlet.java

---
+++ src/main/java/com/RegisterServlet.java
@@ -0,0 +1,19 @@
+package com.example.servlet;
+
+import com.example.model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        User user = new User(name);
+        response.getWriter().println("User registered: " + user.getName());
+    }
+}
